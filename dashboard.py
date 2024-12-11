1aacc9da1231ed66f4e6106a8d884c068a1c4821
valecase4 <valeriocaselli50@gmail.com>
2024-12-11 09:13:34 +0100

    Created a filter where user enters a distance and the app returns all the workouts with a distance that is equal to or greater than the user input.

--- dashboard.py
+++ dashboard.py
@@ -9,5 +9,38 @@ df = pd.read_csv("data/raw/training_data.csv")
 
 app = Dash()
 
+app.layout = html.Div([
+    html.H1("Filter workouts by minimum distance"),
+    html.Div(
+        id='input-div',
+        children=[
+            dcc.Input(id='my-input', placeholder='Distance (km)', type='number', min=0, max=50),
+        ]
+    ),
+    html.Div(id='workout-list')
+])
+
+@callback(
+    Output(component_id='workout-list', component_property='children'),
+    Input(component_id='my-input', component_property='value')
+)
+def show_filtered_workouts(input_value):
+    filtered = []
+    if input_value is not None:
+        for _, row in df.iterrows():
+            if row['Distance (km)'] >= int(input_value):
+                filtered.append(
+                    html.Div(
+                        children=[
+                            html.H4(f"Workout ID: {row['Workout ID']}"),
+                            html.P(f"Date: {row['Date']}"),
+                            html.P(f"Distance: {row['Distance (km)']} km"),
+                            html.P(f"Calories: {row['Calories (kcal)']}"),
+                        ],
+                        className='workout-div'
+                    )
+                )
+        return filtered
+
 if __name__ == '__main__':
     app.run_server(debug=True)
