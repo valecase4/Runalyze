07dd693a3eb9f5680455633da8064e30893a3adf
valecase4 <valeriocaselli50@gmail.com>
2024-12-11 12:06:14 +0100

    First section: created a pie chart that shows the number of workouts per year.

--- src/dashboard.py
+++ src/dashboard.py
@@ -1,7 +1,8 @@
 from dash import Dash, html, dcc, callback, Output, Input
 import plotly.express as px
 import pandas as pd
-from utils import total_km, total_calories, total_workouts
+from utils import total_km, total_calories, total_workouts, get_workouts_per_year
+from graphs import workouts_per_year
 
 df = pd.read_csv("../data/raw/training_data.csv")
 
@@ -11,11 +12,11 @@ app.layout = html.Div([
     html.Div(
         id="section1",
         children=[
-            html.H1("Workout Data Overview"),
+            html.H1("Workout Data Analysis"),
             html.Div(id='stats-div', children=[
                 html.Div([
                     html.H3("Total Distance (km):"),
-                    html.Div(className='display-total', children=total_km(df))
+                    html.Div(className='display-total', id='total-km', children=total_km(df))
                 ], className='card'),
                 html.Div([
                     html.H3("Total Calories Burned:"),
@@ -25,7 +26,16 @@ app.layout = html.Div([
                     html.H3("Workouts Performed:"),
                     html.Div(className='display-total', children=total_workouts(df))
                 ], className='card')
-            ])
+            ]),
+            html.Div(
+                id='graph-section1',
+                children=[
+                    html.H3("Number of Workouts per Year"),
+                    html.Div(id='graph-container', children=[
+                        dcc.Graph(figure=workouts_per_year(df), id='workouts-per-year-graph')
+                    ])
+                ]
+            )
         ]
     )
 ])
--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 def total_km(df):
     return round(df['Distance (km)'].sum(), 2)
 
@@ -6,3 +8,8 @@ def total_calories(df):
 
 def total_workouts(df):
     return df.shape[0]
+
+def get_workouts_per_year(df):
+    df['Date'] = pd.to_datetime(df['Date'])
+    workouts_per_year = df.groupby(df['Date'].dt.year).size()
+    return workouts_per_year
